1cc37b4f2636297beb70041dce127851790d451c
seomike <seomike@gmail.com>
2018-04-23 21:05:43 -0500

    Initial commit

--- config/connections.php
+++ config/connections.php
@@ -1,7 +1,7 @@
 <?php
 /*
  * -------------------------------------------------
- * Database connections
+ * Database connection definitions
  * -------------------------------------------------
  */
 return [
