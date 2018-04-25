8e02cee41c34cfbfec8e703bf2a4ede3ea4d1be3
seomike <seomike@gmail.com>
2018-04-24 19:31:42 -0500

    cleanup

--- tasks/Tasks.php
+++ tasks/Tasks.php
@@ -7,14 +7,6 @@
  */
 
 // Scheduled task
-$task->running('RunningSample', [
-    'at' => function ($task) {
-        return $task->everyMinute();
-    },
-    'uses' => 'CoreServices'
-]);
-
-// Scheduled task
 $task->scheduled('ScheduledSample', [
     'at' => function ($task) {
         return $task->everyMinute();
